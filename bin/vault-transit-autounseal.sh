1895a7c109333f2156ef17df33deead8a4934416
Nick Cabatoff <ncabatoff@hashicorp.com>
2019-04-17 15:51:38 -0400

    Improve readability.

--- bin/vault-transit-autounseal.sh
+++ bin/vault-transit-autounseal.sh
@@ -6,72 +6,97 @@ set -e
 
 tmpdir=`mktemp -d /tmp/vaultXXXXXX`
 
-vault secrets enable transit
-vault write -force /transit/keys/vault2_autounseal
+function setup_primary() {
+    echo "* setup_primary"
+    vault secrets disable transit >/dev/null 2>&1 || true
+    vault secrets enable transit
+    vault write -force /transit/keys/vault2_autounseal
 
-cat - > $tmpdir/primary-policy.hcl <<EOF
+    cat - > ${tmpdir}/primary-policy.hcl <<EOF
 path "/transit/encrypt/vault2_autounseal" {
-	capabilities = ["update"]
+    capabilities = ["update"]
 }
 path "/transit/decrypt/vault2_autounseal" {
-	capabilities = ["update"]
+    capabilities = ["update"]
 }
 EOF
-vault policy write secautounseal $tmpdir/primary-policy.hcl
+    vault policy write secautounseal ${tmpdir}/primary-policy.hcl
+}
 
-cat - > $tmpdir/vault-secondary.hcl <<EOF
-ui = true
+function setup_secondary() {
+    echo "* setup_secondary: writing config tp ${tmpdir}"
+    cat - > ${tmpdir}/vault-secondary.hcl <<EOF
+pid_file = "${tmpdir}/pidfile"
 
 listener "tcp" {
     address = "0.0.0.0:8202"
     tls_disable = 1
 }
 
-storage "consul" {
-    path = "vault2/"
+storage "file" {
+    path = "${tmpdir}/vaultdata"
 }
 
 seal "transit" {
-  address            = "$VAULT_ADDR"
+  address            = "${VAULT_ADDR}"
   disable_renewal    = "false"
   key_name           = "vault2_autounseal"
   mount_path         = "transit/"
 }
 EOF
-
-SEC_TOKEN=$(vault token create -policy=secautounseal -format=json |jq -r .auth.client_token)
-
-VAULT_TOKEN=$SEC_TOKEN vault server -config=$tmpdir/vault-secondary.hcl -log-level=debug > $tmpdir/secondary.log 2>&1 &
-
-while ! nc -z localhost 8202; do
-  sleep 1
-  echo -n '.'
-done
-echo
-vault2_root_token=$(VAULT_TOKEN=$SEC_TOKEN VAULT_ADDR=http://localhost:8202 vault operator init -key-shares=1 -key-threshold=1 -format=json|jq -r .root_token)
-VAULT_ADDR=http://localhost:8202 VAULT_TOKEN=$vault2_root_token vault secrets enable kv
-VAULT_ADDR=http://localhost:8202 VAULT_TOKEN=$vault2_root_token vault kv put kv/foo val=1
-
-kill %1
-while nc -z localhost 8202; do
-  sleep 1
-  echo -n '.'
-done
-echo
-
-VAULT_TOKEN=$SEC_TOKEN vault server -config=$tmpdir/vault-secondary.hcl -log-level=debug > $tmpdir/secondary-unseal.log 2>&1 &
-function cleanup {
-  kill %1
-  rm -rf $tmpdir
-  vault secrets disable transit
-  vault policy delete secautounseal
-  consul kv delete -recurse vault2
 }
-trap cleanup EXIT
-while ! nc -z localhost 8202; do
-  sleep 1
-  echo -n '.'
-done
-echo
 
-VAULT_ADDR=http://localhost:8202 VAULT_TOKEN=$vault2_root_token vault kv get kv/foo
+function launch_secondary() {
+    echo "* launch_secondary"
+    vault server -config=${tmpdir}/vault-secondary.hcl -log-level=debug > ${tmpdir}/secondary.log 2>&1 &
+
+    while ! nc -z localhost 8202; do
+      sleep 1
+      echo -n '.'
+    done
+    echo
+}
+
+function kill_secondary() {
+    echo "* kill_secondary"
+    kill $(cat ${tmpdir}/pidfile)
+
+    while nc -z localhost 8202 || -f ${tmpdir}/pidfile; do
+      sleep 1
+      echo -n '.'
+    done
+    sleep 1
+    echo
+}
+
+function cleanup {
+  kill_secondary
+  #rm -rf ${tmpdir}
+}
+
+# ------------------------------------------------
+# ------------- Script starts here ---------------
+# ------------------------------------------------
+
+trap cleanup EXIT
+
+setup_primary
+
+VAULT_TOKEN=$(vault token create -policy=secautounseal -field=token)
+
+setup_secondary
+
+VAULT_ADDR=http://localhost:8202
+
+launch_secondary
+
+VAULT2_ROOT_TOKEN=$(
+  vault operator init -key-shares=1 -key-threshold=1 -format=json |
+  jq -r .root_token)
+VAULT_TOKEN=${VAULT2_ROOT_TOKEN} vault secrets enable kv
+VAULT_TOKEN=${VAULT2_ROOT_TOKEN} vault kv put kv/foo val=1
+
+kill_secondary
+launch_secondary
+
+VAULT_TOKEN=${VAULT2_ROOT_TOKEN} vault kv get kv/foo
